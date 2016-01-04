f7c1e4caa53bdb65339d1d05f7bbda2a2bae63f9
Ahsen Jaffer <ajaffer@vmware.com>
2016-01-03 21:24:29 -0800

    send location coordinates

--- app/src/main/java/com/navnex/pingger/ScrollingActivity.java
+++ app/src/main/java/com/navnex/pingger/ScrollingActivity.java
@@ -1,5 +1,9 @@
 package com.navnex.pingger;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -15,6 +19,7 @@ import org.springframework.http.HttpBasicAuthentication;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -25,12 +30,45 @@ import java.util.Map;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+
+    // Define a listener that responds to location updates
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            new HttpRequestTask().execute(location);
+//            ping(location.getLatitude(), location.getLongitude());
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
+
+    String locationProvider = LocationManager.NETWORK_PROVIDER;
+    // Or, use GPS location data:
+    // String locationProvider = LocationManager.GPS_PROVIDER;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+//        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+//        ping(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+//        new HttpRequestTask().doInBackground(lastKnownLocation);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,10 +76,14 @@ public class ScrollingActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                new HttpRequestTask().execute();
+//                new HttpRequestTask2().execute();
+                Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+                new HttpRequestTask().execute(lastKnownLocation);
             }
         });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,12 +106,33 @@ public class ScrollingActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private static final String HOST = "http://192.168.1.6:8080";
 
     private static class Traveller {
-        String email;
-        String lat;
-        String lng;
+        String email, lat, lng;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getLat() {
+            return lat;
+        }
+
+        public void setLat(String lat) {
+            this.lat = lat;
+        }
+
+        public String getLng() {
+            return lng;
+        }
+
+        public void setLng(String lng) {
+            this.lng = lng;
+        }
 
         Traveller(String email, String lat, String lng) {
             this.email = email;
@@ -78,33 +141,65 @@ public class ScrollingActivity extends AppCompatActivity {
         }
     }
 
-    private class HttpRequestTask extends AsyncTask<Void, Void, Void> {
+    private static class Greeting {
+
+        private String id;
+        private String content;
+
+        public String getId() {
+            return this.id;
+        }
+
+        public String getContent() {
+            return this.content;
+        }
+
+    }
+
+    private class HttpRequestTask2 extends AsyncTask<Void, Void, Greeting> {
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Greeting doInBackground(Void... params) {
             try {
-                ping(123,343);
+                final String url = "http://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
             } catch (Exception e) {
-                System.err.print(e.getMessage());
+                System.out.println("MainActivity :" + e.getMessage());
             }
 
             return null;
         }
 
-        private void ping(double lat, double lng) {
-            final String uri = HOST + "/api/traveller/location";
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            System.out.println(greeting);
+//            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+//            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+//            greetingIdText.setText(greeting.getId());
+//            greetingContentText.setText(greeting.getContent());
+        }
 
-            Map<String, String> params = new HashMap<String, String>();
+    }
+    private static final String IP_ADDRESS = "73.15.199.242";
+
+    private void ping(double lat, double lng) {
+        final String uri = String.format("http://%d:8080/api/traveller/location", IP_ADDRESS);
+
+//            Map<String, String> params = new HashMap<String, String>();
 //        params.put("id", "2");
 
-            Traveller updatedTraveller = new Traveller("ahsen.jaffer@gmail.com", lat+"", lng+"");
+        Traveller updatedTraveller = new Traveller("Ahsen Jaffer <ahsen.jaffer@gmail.com>", lat+"", lng+"");
 
-            RestTemplate restTemplate = new RestTemplate();
-            restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
-            try {
-                restTemplate.put(uri, updatedTraveller, params);
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        try {
+//                restTemplate.put(uri, updatedTraveller, params);
+            restTemplate.put(uri, updatedTraveller);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
 //        HttpAuthentication httpAuthentication = new HttpBasicAuthentication("admin", "admin");
 //        HttpHeaders requestHeaders = new HttpHeaders();
@@ -120,7 +215,44 @@ public class ScrollingActivity extends AppCompatActivity {
 //            System.err.println(e.getMessage());
 //        }
 
+    }
+
+    private class HttpRequestTask extends AsyncTask<Location, Void, Void> {
+        private void setupLocationListener() {
+            // Acquire a reference to the system Location Manager
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            // Define a listener that responds to location updates
+            LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    // Called when a new location is found by the network location provider.
+                    ping(location.getLatitude(), location.getLongitude());
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                public void onProviderEnabled(String provider) {}
+
+                public void onProviderDisabled(String provider) {}
+            };
+
+            // Register the listener with the Location Manager to receive location updates
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         }
+
+        @Override
+        protected Void doInBackground(Location... params) {
+            try {
+                Location location = params[0];
+                ping(location.getLatitude(), location.getLongitude());
+//                setupLocationListener();
+            } catch (Exception e) {
+                System.err.print(e.getMessage());
+            }
+
+            return null;
+        }
+
 
 //        @Override
 //        protected void onPostExecute(Greeting greeting) {
